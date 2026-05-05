d2ff02811352aa84e37e123eda39f7bfa9353ee5
Robota Test <test@example.com>
2026-05-05 18:16:04 +0900

    refactor(playground): split template gallery data

---
+++ packages/agent-playground/src/components/playground/template-gallery-data/index.ts
@@ -0,0 +1,3 @@
+export { categoryIcons, difficultyColors, providerIcons } from './display';
+export { templates } from './templates';
+export type { ITemplate, ITemplateGalleryProps } from './types';
